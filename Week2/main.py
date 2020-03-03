c3525c98e067ac31457deb0fd9a44af272210dfd
RogerCasalsVilardell <43968704+RogerCasalsVilardell@users.noreply.github.com>
2020-03-03 13:28:30 +0100

    First commit week 2

---
+++ Week2/main.py
@@ -0,0 +1,10 @@
+from src.tasks import task2
+
+
+
+def main():
+	task2()
+
+
+if __name__ == '__main__':
+	main()
---
+++ Week2/src/tasks.py
@@ -0,0 +1,52 @@
+import os
+from glob import glob
+import numpy as np
+import cv2
+import random
+
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor, DefaultTrainer
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer, ColorMode
+from detectron2.data import MetadataCatalog, build_detection_test_loader
+from detectron2.evaluation import COCOEvaluator, inference_on_dataset
+
+from utils import dataloader
+
+
+DATASET_PATH = '/home/grupo07/MIT_split'
+SAVE_PATH = './results'
+if not os.path.exists(SAVE_PATH):
+    os.makedirs(SAVE_PATH)
+
+
+def task2():
+    # TODO: Load dataset
+    DL = dataloader()
+    dataset = DL.load_data()
+
+    # Load model and checkpoint
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml"))
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/faster_rcnn_R_50_FPN_3x.yaml")
+    cfg.DATASETS.TEST = ("MIT_split_test")
+    predictor = DefaultPredictor(cfg)
+
+    # Qualitative results: visualize some prediction results on MIT_split dataset
+    for i, img_path in enumerate(random.sample(dataset["test"], 3)):
+        img = cv2.imread(img_path)
+        outputs = predictor(img)
+        v = Visualizer(
+            img[:, :, ::-1],
+            metadata=metadata
+            scale=0.8, 
+            instance_mode=ColorMode.IMAGE_BW)
+        v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+        cv2.imwrite(os.path.join(SAVE_PATH, 'Task2_FasterRCNN_inf_'+str(i)+'.png'), v.get_image()[:, :, ::-1])
+
+    # Quantitative results: compute AP
+    trainer = DefaultTrainer(cfg)
+    evaluator = COCOEvaluator("MIT_split_test", cfg, False)
+    val_loader = build_detection_test_loader(cfg, "MIT_split_test")
+    inference_on_dataset(trainer.model, val_loader, evaluator)
