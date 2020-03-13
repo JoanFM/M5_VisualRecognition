40329fe73d3e2fd754d65e6dfd6bc74b74e5a17c
paudom <pavdom72@gmail.com>
2020-03-13 17:23:16 +0100

    Added KITTIMOTS_Dataloader and sketch the tasks functions.

--- Week3/src/tasks.py
+++ Week3/src/tasks.py
@@ -11,9 +11,10 @@ from detectron2.config import get_cfg
 from detectron2.utils.visualizer import Visualizer, ColorMode
 from detectron2.data import MetadataCatalog, build_detection_test_loader
 from detectron2.evaluation import COCOEvaluator, inference_on_dataset
+from visualizer import plot_losses, show_results
+from hooks import ValidationLoss
 
 from .utils import KITTIMOTS_Inference_Dataloader
-
 
 SAVE_PATH = './results_week_3'
 
@@ -22,10 +23,12 @@ def KITTIMOTS_inference_task(model_name, model_file):
     if not os.path.exists(path):
         os.makedirs(path)
     # Loading training and test examples
-    dataloader = KITTIMOTS_Inference_Dataloader()
+    print('Loading Data...')
+    dataloader = Inference_Dataloader()
     dataset = dataloader.load_data()
 
     # Load model and checkpoint
+    print('Loading Model and Checkpoint..')
     cfg = get_cfg()
     cfg.merge_from_file(model_zoo.get_config_file(model_file))
     cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
@@ -33,6 +36,7 @@ def KITTIMOTS_inference_task(model_name, model_file):
     predictor = DefaultPredictor(cfg)
 
     # Qualitative results: visualize some prediction results on MIT_split dataset
+    print('Getting Qualitative Results...')
     for i, img_path in enumerate(dataset['test'][:20]):
         img = cv2.imread(img_path)
         outputs = predictor(img)
@@ -43,3 +47,86 @@ def KITTIMOTS_inference_task(model_name, model_file):
             instance_mode=ColorMode.IMAGE)
         v = v.draw_instance_predictions(outputs['instances'].to('cpu'))
         cv2.imwrite(os.path.join(path, 'Inference_' + model_name + '_inf_' + str(i) + '.png'), v.get_image()[:, :, ::-1])
+
+def KITTIMOTS_evaluation_task(model_name, model_file):
+    path = os.path.join(SAVE_PATH, 'eval_inf_task', model_name)
+    if not os.path.exists(path):
+        os.makedirs(path)
+    # Load Data
+    print('Loading Data.')
+    dataloader = KITTIMOTS_Dataloader()
+    def kitti_train(): return dataloader.get_dicts(train_flag=True)
+    def kitti_test(): return dataloader.get_dicts(train_flag=False)
+    DatasetCatalog.register("KITTIMOTS_train", kitti_train)
+    MetadataCatalog.get("KITTIMOTS_train").set(thing_classes=[k for k,_ in CATEGORIES.items()])
+    DatasetCatalog.register("KITTIMOTS_test", kitti_test)
+    MetadataCatalog.get("KITTIMOTS_test").set(thing_classes=[k for k,_ in CATEGORIES.items()])
+
+    # Load MODEL and Configuration
+    print('Loading Model.')
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file(model_file))
+    cfg.DATASETS.TRAIN = ('KITTIMOTS_train',)
+    cfg.DATASETS.TEST = ('KITTIMOTS_test',)
+    cfg.DATALOADER.NUM_WORKERS = 0
+    cfg.OUTPUT_DIR = SAVE_PATH
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(model_file)
+    trainer = DefaultTrainer(cfg) 
+    trainer.resume_or_load(resume=False)
+
+    # Evaluation
+    print('Evaluating......')
+    evaluator = COCOEvaluator('KITTIMOTS_test', cfg, False, output_dir="./output/")
+    trainer.test(cfg, trainer.model, evaluators=[evaluator])
+    
+def KITTIMOTS_training_and_evaluation_task(model_name,model_file):
+    path = os.path.join(SAVE_PATH, 'train_task', model_name)
+    if not os.path.exists(path):
+        os.makedirs(path)
+    # Load Data
+    print('Loading Data.')
+    dataloader = KITTIMOTS_Dataloader()
+    def kittmots_train(): return dataloader.get_dicts(train_flag=True)
+    def kittimots_test(): return dataloader.get_dicts(train_flag=False)
+    DatasetCatalog.register("KITTIMOTS_train", kittmots_train)
+    MetadataCatalog.get("KITTIMOTS_train").set(thing_classes=[k for k,_ in CATEGORIES.items()])
+    DatasetCatalog.register("KITTIMOTS_test", kittimots_test)
+    MetadataCatalog.get("KITTIMOTS_test").set(thing_classes=[k for k,_ in CATEGORIES.items()])
+
+    # PARAMETERS
+    print('Loading Model.')
+    cfg = get_cfg()
+    cfg.merge_from_file(model_zoo.get_config_file(model_file))
+    cfg.DATASETS.TRAIN = ('KITTIMOTS_train',)
+    cfg.DATASETS.TEST = ('KITTIMOTS_test',)
+    cfg.DATALOADER.NUM_WORKERS = 0
+    cfg.OUTPUT_DIR = SAVE_PATH
+    cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url(model_file)
+    cfg.SOLVER.IMS_PER_BATCH = 4
+    cfg.SOLVER.BASE_LR = 0.00025
+    cfg.SOLVER.MAX_ITER = NUM_IMGS // cfg.SOLVER.IMS_PER_BATCH + 1 
+    cfg.MODEL.ROI_HEADS.BATCH_SIZE_PER_IMAGE = 256
+    cfg.MODEL.ROI_HEADS.NUM_CLASSES = 2
+
+    # Training
+    print('Training....')
+    os.makedirs(cfg.OUTPUT_DIR, exist_ok=True)
+    trainer = DefaultTrainer(cfg)
+    val_loss = ValidationLoss(cfg)
+    trainer.register_hooks([val_loss])
+    trainer._hooks = trainer._hooks[:-2] + trainer._hooks[-2:][::-1]
+    trainer.resume_or_load(resume=False)
+    trainer.train()
+
+    # EVALUATION
+    print('Evaluating....')
+    evaluator = COCOEvaluator("KITTIMOTS_test", cfg, False, output_dir="./output/")
+    trainer.model.load_state_dict(val_loss.weights)
+    trainer.test(cfg, trainer.model, evaluators=[evaluator])
+    
+    # Qualitative results
+    print('Inference on trained model')
+    predictor = DefaultPredictor(cfg)
+    predictor.model.load_state_dict(trainer.model.state_dict())
+    plot_losses(cfg)
+    show_results(cfg, kittimots_test(), predictor, samples=10)
--- Week3/src/utils.py
+++ Week3/src/utils.py
@@ -4,48 +4,19 @@ import numpy as np
 import cv2
 
 from detectron2.structures import BoxMode
+from pycocotools import coco
 
 KITTI_CATEGORIES = {
-    'Car': 0,
-    'Van': 1,
-    'Truck': 2,
-    'Pedestrian': 3,
-    'Person_sitting': 4,
-    'Cyclist': 5,
-    'Tram': 6,
-    'Misc': 7,
-    'DontCare': 8
+    'Car': 1,
+    'Pedestrian': 2,
 }
-MIT_DATA_DIR = '/home/mcv/datasets/MIT_split/'
+
 KITTIMOTS_DATA_DIR = '/home/mcv/datasets/KITTI-MOTS/'
 KITTIMOTS_TRAIN_IMG = KITTIMOTS_DATA_DIR+'training/image_02'
 KITTIMOTS_TEST_IMG = KITTIMOTS_DATA_DIR+'testing/image_02'
+KITTIMOTS_TRAIN_LABEL = KITTIMOTS_DATA_DIR+'instances_txt'
 
-"""
-Labels of KITTI -- Meaning
-There are no labels for test images 
-----------------------------------------------------------------------------
-   1    type         Describes the type of object: 'Car', 'Van', 'Truck',
-                     'Pedestrian', 'Person_sitting', 'Cyclist', 'Tram',
-                     'Misc' or 'DontCare'
-   1    truncated    Float from 0 (non-truncated) to 1 (truncated), where
-                     truncated refers to the object leaving image boundaries
-   1    occluded     Integer (0,1,2,3) indicating occlusion state:
-                     0 = fully visible, 1 = partly occluded
-                     2 = largely occluded, 3 = unknown
-   1    alpha        Observation angle of object, ranging [-pi..pi]
-   4    bbox         2D bounding box of object in the image (0-based index):
-                     contains left, top, right, bottom pixel coordinates
-   3    dimensions   3D object dimensions: height, width, length (in meters)
-   3    location     3D object location x,y,z in camera coordinates (in meters)
-   1    rotation_y   Rotation ry around Y-axis in camera coordinates [-pi..pi]
-   1    score        Only for results: Float, indicating confidence in
-                     detection, needed for p/r curves, higher is better.
-
-Example: Pedestrian 0.00 0 -0.20 712.40 143.00 810.73 307.92 1.89 0.48 1.20 1.84 1.47 8.41 0.01
-"""
-
-class KITTIMOTS_Inference_Dataloader():
+class Inference_Dataloader():
     def __init__(self):
         self.train_paths = glob(os.path.join(KITTIMOTS_TRAIN_IMG,'*','*'))
         self.test_paths = glob(os.path.join(KITTIMOTS_TEST_IMG,'*','*'))
@@ -55,3 +26,54 @@ class KITTIMOTS_Inference_Dataloader():
             'train': self.train_paths,
             'test': self.test_paths
         }
+
+class KITTI_Dataloader():
+    def __init__(self, split_perc=0.8):
+        if not os.path.isdir(KITTIMOTS_TRAIN_IMG):
+            raise Exception('The image directory is not correct.')
+        if not os.path.isdir(KITTIMOTS_TRAIN_LABEL):
+            raise Exception('The masks directory is not correct.')
+        label_paths = sorted(glob(KITTI_TRAIN_LABEL+os.sep+'*.txt'))
+        self.label_indices = ['{0:04d}'.format(l) for l in range(len(label_paths))]
+        split_point = int(len(img_indices)*split_perc)
+        self.train_labels = self.label_indices[:split_point]
+        self.test_labels = self.label_indices[split_point:]
+    
+    def get_dicts(self, train_flag=False):
+        sequences = self.train_indices if train_flag is True else self.test_indices
+        dataset_dicts = []
+        for seq in sequences:
+            seq_dicts = []
+            image_paths = sorted(glob(KITTIMOTS_TRAIN_IMG+os.sep+seq+os.sep+'*.png'))
+            num_frames = len(image_paths)
+            label_path = KITTIMOTS_TRAIN_LABEL+os.sep+seq+'.txt'
+            with open(label_path,'r') as file:
+                lines = file.readlines()
+                lines = [l.split(' ') for l in lines]
+            for k in range(num_frames):
+                frame_lines = [l for l in lines if int(l[0]) == k]
+                frame_annotations = []
+                h, w = int(frame_lines[0][3]), int(frame_lines[0][4])
+                for detection in frame_lines:
+                    rle = {
+                        'counts': detection[-1].strip(),
+                        'size': [h, w]
+                    }
+                    bbox = coco.maskUtils.toBbox(rle)
+                    annotation = {
+                        'category_id': int(detection[1])%1000 #detection[2]?
+                        'bbox_mode': BoxMode.XYXY_ABS,
+                        'bbox':bbox
+                    }
+                    frame_annotations.append(annotation)
+                filename = '{0:06d}.png'.format(k)
+                img_dict = {
+                    'file_name': os.path.join(KITTIMOTS_TRAIN_IMG,seq,filename),
+                    'image_id': k+(int(seq)*1e3),
+                    'height': h,
+                    'width': w,
+                    'annotations': frame_annotations
+                }
+                seq_dicts.append(img_dict)
+            dataset_dicts += seq_dicts
+        return dataset_dicts
