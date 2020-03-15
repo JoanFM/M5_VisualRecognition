3531ff21c4040b3d3d854ee61c142e1cfb660c88
paudom <pavdom72@gmail.com>
2020-03-15 09:42:37 +0100

    Mal de coco

--- Week3/src/utils.py
+++ Week3/src/utils.py
@@ -10,12 +10,8 @@ from detectron2.structures import BoxMode
 from pycocotools import coco
 
 KITTI_CATEGORIES = {
-    'Car': 0,
-    'Pedestrian': 1,
-}
-COCO_CATTEGORIES = {
-    0: 2,
-    1: 0
+    'Car': 1,
+    'Pedestrian': 2,
 }
 
 KITTIMOTS_DATA_DIR = '/home/mcv/datasets/KITTI-MOTS/'
@@ -99,9 +95,10 @@ class KITTIMOTS_Dataloader():
                         }
                         bbox = coco.maskUtils.toBbox(rle)
                         bbox = [int(item) for item in bbox]
+                        print(detection[1],type(detection[1]))
                         category_id = int(detection[1])//1000
                         annotation = {
-                            'category_id': COCO_CATTEGORIES[category_id], #detection[2]?
+                            'category_id': category_id, #detection[2]?
                             'bbox_mode': BoxMode.XYXY_ABS,
                             'bbox':bbox
                         }
