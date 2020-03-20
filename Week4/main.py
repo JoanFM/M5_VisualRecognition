1ecb7d9c32b46031b3f8a834820d0997e5e5ec72
RogerCasalsVilardell <43968704+RogerCasalsVilardell@users.noreply.github.com>
2020-03-20 02:14:46 +0100

    Added Cityscapes model

--- Week4/main.py
+++ Week4/main.py
@@ -9,6 +9,7 @@ def main():
         - COCO-InstanceSegmentation/mask_rcnn_R_101_C4_3x.yaml
         - COCO-InstanceSegmentation/mask_rcnn_R_101_DC5_3x.yaml
         - COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml
+        - Cityscapes/mask_rcnn_R_50_FPN.yaml
     """
 
     # -- TASK A -- #
@@ -18,6 +19,7 @@ def main():
     # task_a("MaskRCNN_R_101_C4", "COCO-InstanceSegmentation/mask_rcnn_R_101_C4_3x.yaml")
     # task_a("MaskRCNN_R_101_DC5", "COCO-InstanceSegmentation/mask_rcnn_R_101_DC5_3x.yaml")
     # task_a("MaskRCNN_R_101_FPN", "COCO-InstanceSegmentation/mask_rcnn_R_101_FPN_3x.yaml")
+    # task_a("MaskRCNN_R_50_FPN_Cityscapes", "Cityscapes/mask_rcnn_R_50_FPN.yaml")
 
     
 if __name__ == '__main__':
